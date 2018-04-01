b56c9a060c63111aad498dca79c7f4f6fdd3938a
Alex Cheng <cheng.alex5@gmail.com>
2018-04-01 13:51:45 -0500

    minor syntax change

--- week1/average.cpp
+++ week1/average.cpp
@@ -2,21 +2,17 @@
  * Date: 3/29/18
  * Author: Alex Cheng
  * Description: A program that outputs the average of 5 input numbers
-** ***********************************************/
+ **************************************************/
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-	double a;
-	double b;
-	double c;
-	double d;
-	double e;
-	cout << "Please enter five numbers." << endl;
-	cin >> a >> b >> c >> d >> e;
-	cout << "The average of those numbers is: " << endl;
-	cout << (a+b+c+d+e)/5 << endl;
+  double a, b, c, d, e;
+  cout << "Please enter five numbers." << endl;
+  cin >> a >> b >> c >> d >> e;
+  cout << "The average of those numbers is: " << endl;
+  cout << (a + b + c + d + e) / 5 << endl;
 
-	return 0;
+  return 0;
 }
--- week1/tempConvert.cpp
+++ week1/tempConvert.cpp
@@ -1,18 +1,19 @@
 /************************************************
  * Date: 3/29/18
  * Author: Alex Cheng
- * Description: A program that outputs the given temperature(celsius)  in Fahrenheit.
- * ***********************************************/ 
-#include<iostream>
+ * Description: A program that outputs the given temperature(celsius) in
+ * Fahrenheit.
+ * ***********************************************/
+#include <iostream>
 using namespace std;
 
 int main() {
-	double celsius;	
+  double celsius;
 
-	cout << "Please enter a Celsius temperature." << endl;
-	cin >> celsius;
-	cout << "The equivalent Fahrenheit temperature is: " << endl;
-	cout << ((9.0/5)*celsius) + 32 << endl;
+  cout << "Please enter a Celsius temperature." << endl;
+  cin >> celsius;
+  cout << "The equivalent Fahrenheit temperature is: " << endl;
+  cout << (9 * celsius / 5) + 32 << endl;
 
-	return 0;
+  return 0;
 }
