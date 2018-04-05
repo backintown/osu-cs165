d9deaba63954b9944235fdf41c98dc729c432781
Alex Cheng <cheng.alex5@gmail.com>
2018-04-05 14:22:24 -0500

    project 2a initial commit

---
+++ week2/minmax.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+  int max, min, numIntegers, input;
+  cout << "How many integers would you like to enter?" << endl;
+  cin >> numIntegers;
+  cout << "Please enter " << numIntegers << " integers." << endl;
+  for (int i = 1; i <= numIntegers; i++) {
+    cin >> input;
+    if (i == 1) {
+      max = input;
+      min = input;
+    } else if (input <= min) {
+      min = input;
+    } else if (input >= max) {
+      max = input;
+    }
+  }
+
+  cout << "min: " << min << endl;
+  cout << "max: " << max << endl;
+}
