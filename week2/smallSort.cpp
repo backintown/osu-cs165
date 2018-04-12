63cdffeab4cc0b4b9a1bb712e8285740adfaeefb
Alex Cheng <cheng.alex5@gmail.com>
2018-04-11 19:50:03 -0500

    remove unnecessary condition

--- week2/smallSort.cpp
+++ week2/smallSort.cpp
@@ -6,53 +6,42 @@
  ** The function places the lowest value in first and so on.
  **************************************/
 
-/*
 #include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
 
 void smallSort(int &first, int &second, int &third);
-*/
 
 void smallSort(int &first, int &second, int &third) {
   int tmp;
-  // first is the greatest
-  if (first > second && first > third) {
-    // flip first and third
-    tmp = first;
-    first = third;
-    third = tmp;
-    // then check if second is greater than third
-    if (second > third) {
-      tmp = second;
-      second = third;
-      third = tmp;
-    }
-  }
-  // first is greater than second but not greater than third
+
   if (first > second) {
     tmp = first;
     first = second;
     second = tmp;
+    cout << first << ", " << second << ", " << third << endl;
   }
-  // first is greater than third but not greater than second
+
   if (first > third) {
     tmp = first;
     first = third;
     third = tmp;
+    cout << first << ", " << second << ", " << third << endl;
   }
-  // first is less than second and third, second is greater than third
+
   if (second > third) {
     tmp = second;
     second = third;
     third = tmp;
+    cout << first << ", " << second << ", " << third << endl;
   }
 }
 
-/* int main() {
-  int a = 3, b = 2, c = 1;
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
   smallSort(a, b, c);
   cout << a << ", " << b << ", " << c << endl;
   return 0;
-} */
+}
